3b04d95a44a97b2e126828ceaa35ac50ba7e812e
Eferka <francis.cadorette.1@ens.etsmtl.ca>
2019-01-26 05:10:36 -0500

    yo ca marche

--- Unity Project/Assets/Scripts/Object/Interaction/Pushable.cs
+++ Unity Project/Assets/Scripts/Object/Interaction/Pushable.cs
@@ -15,22 +15,32 @@ public class Pushable : Interactable
                    // | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezePositionX;
                 transform.parent = player.transform;
                 var animator = GetComponentInParent<Animator>();
-                if (animator != null)
+               /* if (animator != null)
                 {
                     //animator.SetBool("Grab", true);
                     Debug.Log("You grabbed " + gameObject.name);
-                }
+                }*/
                 if (Input.GetKeyDown("w"))
                 {
-                    //animator.SetBool("Push", true);
+                    player.GetComponentInChildren<Animator>().SetBool("Push", true);
                     Debug.Log("You pushed " + gameObject.name);
+                }
+                if (Input.GetKeyUp("w"))
+                {
+                    player.GetComponentInChildren<Animator>().SetBool("Push", false);
+                    Debug.Log("You stop pushing " + gameObject.name);
                 }
                 if (Input.GetKeyDown("s"))
                 {
-                    //animator.SetBool("Pull", true);
+                    player.GetComponentInChildren<Animator>().SetBool("Pull", true);
                     Debug.Log("You pulled " + gameObject.name);
                 }
-                if(Input.GetMouseButtonUp(0))
+                if (Input.GetKeyUp("s"))
+                {
+                    player.GetComponentInChildren<Animator>().SetBool("Pull", true);
+                    Debug.Log("You stop pulling " + gameObject.name);
+                }
+                if (Input.GetMouseButtonUp(0))
                 {
                     Debug.Log("Tu marches");
                     transform.parent = null;
