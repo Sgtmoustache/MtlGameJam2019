348338fbab4250b3f241dea3248f66693fb410ca
Eferka <francis.cadorette.1@ens.etsmtl.ca>
2019-01-26 16:15:50 -0500

    night light ez pz

---
+++ Unity Project/Assets/Scripts/Event/PlugTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlugTrigger: TriggerObject
+{
+
+    public bool hasNightLight;
+
+    public override void TriggerEffect()
+    {
+
+        if (Inventory.HasNightlight() || hasNightLight)
+        {
+            if (hasNightLight) Debug.Log("Enfant Jesus");
+            hasNightLight = !hasNightLight;
+            Inventory.ChangeNight();
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hasNightLight = false;
+    }
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Unity Project/Assets/Scripts/Object/Interaction/Pickable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickable : Interactable
+{
+
+    public override void Interact(GameObject player, bool input)
+    {
+        if (input)
+        {
+            player.GetComponentInChildren<Animator>().SetBool("Grab", true);
+            Inventory.ChangeNight();
+            Debug.Log("You collected item light");
+            Destroy(this.gameObject);
+        }
+
+    }
+
+    // Start is called before the first frame update
+    public override void OnStart()
+    {
+        
+    }
+
+    
+
+}
