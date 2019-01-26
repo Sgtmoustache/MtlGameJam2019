15eabf695fa4880036c0f146fd10b0fb34fd3567
Unknown <AM99860@ens.etsmtl.ca>
2019-01-26 09:12:16 -0500

    Main menu stuff

--- Unity Project/Assets/Scripts/Controllers/UI/UIController.cs
+++ Unity Project/Assets/Scripts/Controllers/UI/UIController.cs
@@ -1,17 +1,28 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class UIController : MonoBehaviour
 {
+    public GameObject fader;
+    public float fadeSpeed;
     public void changeScene(string scenename)
     {
-        SceneManager.LoadScene(scenename);
+        //SceneManager.LoadScene(scenename);
+        StartCoroutine(Fade(scenename, fader));
     }
 
     public void changeScene(int sceneId)
     {
         SceneManager.LoadScene(sceneId);
     }
+
+    private IEnumerator Fade(string sceneName, GameObject fadeOut)
+    {
+        while (true)
+        {
+            fader.transform.Translate(Vector3.right * fadeSpeed);
+        }    }
 
     public void exit(int exitCode = 0)
     {
