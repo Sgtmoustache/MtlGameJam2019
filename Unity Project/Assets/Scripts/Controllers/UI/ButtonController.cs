44d84a864949e7acbf7aa8185f7b9a33d89f7e96
Unknown <AM99860@ens.etsmtl.ca>
2019-01-26 14:33:36 -0500

    Updated main menu

--- Unity Project/Assets/Scripts/Controllers/UI/ButtonController.cs
+++ Unity Project/Assets/Scripts/Controllers/UI/ButtonController.cs
@@ -6,16 +6,16 @@ using UnityEngine.UI;
 
 public class ButtonController : MonoBehaviour, IPointerEnterHandler
 {
-    public Image hoverButtonImage;
-    public Image normalButtonImage;
+    public Sprite hoverButtonImage;
+    public Sprite normalButtonImage;
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        GetComponent<Button>().targetGraphic = hoverButtonImage;
+        //GetComponent<Button>().targetGraphic = hoverButtonImage;
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        GetComponent<Button>().targetGraphic = normalButtonImage;
+        //GetComponent<Button>().targetGraphic = normalButtonImage;
     }
 }
