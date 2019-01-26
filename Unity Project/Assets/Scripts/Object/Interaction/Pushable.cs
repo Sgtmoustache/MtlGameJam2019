bf15206e825179328138d171298c7d29c0b0ac83
Eferka <francis.cadorette.1@ens.etsmtl.ca>
2019-01-26 04:04:19 -0500

    push start
    je regle les erreurs a charlot

--- Unity Project/Assets/Scripts/Object/Interaction/Pushable.cs
+++ Unity Project/Assets/Scripts/Object/Interaction/Pushable.cs
@@ -6,11 +6,39 @@ public class Pushable : Interactable
 {
     public override void Interact(GameObject player, bool input)
     {
-        throw new System.NotImplementedException();
+        try
+        {
+            if (input)
+            {
+
+                player.GetComponent<RigidBody>().constraints = RigidbodyConstraints.FreezeRotationY
+                    | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeTranslationX;
+                var animator = GetComponentInParent<Animator>();
+                if (animator != null)
+                {
+                    animator.SetBool("Grab", true);
+                    Debug.Log("You grabbed " + gameObject.name);
+                }
+                if (Input.GetKeyDown("w"))
+                {
+                    animator.SetBool("Push", true);
+                    Debug.Log("You pushed " + gameObject.name);
+                }
+                if (Input.GetKeyDown("s"))
+                {
+                    animator.SetBool("Pull", true);
+                    Debug.Log("You pulled " + gameObject.name);
+                }
+            }
+        }
+        catch (System.Exception)
+        {
+            throw;
+        }
     }
 
     public override void OnStart()
     {
-        throw new System.NotImplementedException();
+        //throw new System.NotImplementedException();
     }
 }
