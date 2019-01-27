09b69e96dabb7a8a14f9f565c108efc076a62f2a
Unknown <AM99860@ens.etsmtl.ca>
2019-01-27 13:31:16 -0500

    Animated bottle

---
+++ Unity Project/Assets/Scripts/Event/BasicAnimationTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicAnimationTrigger : TriggerObject
+{
+    public float audioDelay = 0;
+    public AudioClip AnimationSound;
+
+    public override void TriggerEffect(GameObject player = null)
+    {
+        var animator = GetComponentInParent<Animator>();
+        if (animator != null)
+        {
+            animator.SetBool("Trigger", true);
+            StartCoroutine(PlaySound());
+        }
+    }
+
+    IEnumerator PlaySound()
+    {
+        yield return new WaitForSeconds(audioDelay);
+        if (AnimationSound != null)
+            GetComponent<AudioSource>().PlayOneShot(AnimationSound);
+    }
+}
